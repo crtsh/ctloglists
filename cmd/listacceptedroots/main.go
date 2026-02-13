20d4aeba16ec450bb14d749c1dcc78cc647c2e6c
Rob Stradling <rob@sectigo.com>
2026-02-13 18:07:21 +0000

    Add tool to list the contents of the Accepted Roots maps.

---
+++ cmd/listacceptedroots/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	"github.com/crtsh/ctloglists"
+)
+
+func main() {
+	if err := ctloglists.Load(); err != nil {
+		panic(err)
+	}
+
+	for logID, rootListHash := range ctloglists.LogAcceptedRootsMap {
+		if ctloglists.AcceptedRootsMap[rootListHash] == nil {
+			fmt.Printf("No accepted roots found for log with ID %s\n", hex.EncodeToString(logID[:]))
+		} else {
+			fmt.Printf("\n%s (%s)\n", hex.EncodeToString(logID[:]), hex.EncodeToString(rootListHash[:]))
+			for _, cert := range ctloglists.AcceptedRootsMap[rootListHash].RawCertificates() {
+				fmt.Printf("  %s\n", cert.Subject.String())
+			}
+		}
+	}
+}
