64a4df0e35a18e4fa893dbff628ed4adf8db56af
Rob Stradling <rob@sectigo.com>
2026-02-25 15:35:24 +0000

    Tweak check_for_acceptedroots_changes.sh

--- cmd/acceptedroots/check_for_acceptedroots_changes.sh
+++ cmd/acceptedroots/check_for_acceptedroots_changes.sh
@@ -9,10 +9,10 @@ go run main.go
 mkdir -p ../../files/acceptedroots
 cd ../../files/acceptedroots
 git restore --staged *.pem *.txt
-git rm *.pem *.txt
-rm *.pem *.txt
+git rm -f *.pem *.txt
+rm -f *.pem *.txt
 mv $SCRIPT_DIR/*.pem .
 mv $SCRIPT_DIR/*.txt .
-git add *.pem *.txt
+git add -v *.pem *.txt
 
 cd $CWD
