0473b103d94b9848b07803cb6c5ba1f3a8e362ae
Rob Stradling <rob@sectigo.com>
2026-09-14 14:14:30 +0100

    Don't overwrite a newer log list with an older one.

--- fetch_ct_log_lists.sh
+++ fetch_ct_log_lists.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-LOGLIST=$(mktemp)
-LOGLISTSIG=$(mktemp)
-
 echo
 mkdir -p files/gstatic/v3
-wget -nv -O "$LOGLIST" https://www.gstatic.com/ct/log_list/v3/all_logs_list.json
+wget -nv -N https://www.gstatic.com/ct/log_list/v3/all_logs_list.json
 if [ $? -eq 0 ]; then
-  wget -nv -O "$LOGLISTSIG" https://www.gstatic.com/ct/log_list/v3/all_logs_list.sig
+  wget -nv -N https://www.gstatic.com/ct/log_list/v3/all_logs_list.sig
   if [ $? -eq 0 ]; then
-    openssl pkeyutl -verify -rawin -pubin -inkey files/gstatic/log_list_pubkey.pem -in "$LOGLIST" -sigfile "$LOGLISTSIG"
+    openssl pkeyutl -verify -rawin -pubin -inkey files/gstatic/log_list_pubkey.pem -in all_logs_list.json -sigfile all_logs_list.sig
     if [ $? -eq 0 ]; then
-      mv "$LOGLIST" files/gstatic/v3/all_logs_list.json
-      mv "$LOGLISTSIG" files/gstatic/v3/all_logs_list.sig
+      mv all_logs_list.json files/gstatic/v3/all_logs_list.json
+      mv all_logs_list.sig files/gstatic/v3/all_logs_list.sig
     else
       echo "Signature verification failed for https://www.gstatic.com/ct/log_list/v3/all_logs_list.json"
     fi
@@ -25,27 +22,27 @@ fi
 
 echo
 mkdir -p files/apple
-wget -nv -O "$LOGLIST" https://valid.apple.com/ct/log_list/current_log_list.json
+wget -nv -N https://valid.apple.com/ct/log_list/current_log_list.json
 if [ $? -eq 0 ]; then
-  mv "$LOGLIST" files/apple/current_log_list.json
+  mv current_log_list.json files/apple/current_log_list.json
 else
   echo "Failed to download log list"
 fi
 
 echo
 mkdir -p files/crtsh/v3/all
-wget -nv -O "$LOGLIST" https://crt.sh/v3/logs.json?include=all
+wget -nv -N http://crt.sh/v3/logs.json?include=all
 if [ $? -eq 0 ]; then
-  mv "$LOGLIST" files/crtsh/v3/all/all_logs_list.json
+  mv "logs.json?include=all" files/crtsh/v3/all/all_logs_list.json
 else
   echo "Failed to download log list"
 fi
 
 echo
 mkdir -p files/crtsh/v3/active
-wget -nv -O "$LOGLIST" https://crt.sh/v3/logs.json?include=active
+wget -nv -N http://crt.sh/v3/logs.json?include=active
 if [ $? -eq 0 ]; then
-  mv "$LOGLIST" files/crtsh/v3/active/active_logs_list.json
+  mv "logs.json?include=active" files/crtsh/v3/active/active_logs_list.json
 else
   echo "Failed to download log list"
 fi
